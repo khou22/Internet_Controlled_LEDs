13e939cbd37d0de4f93a4cbae564d7505abbd661
Kevin Hou <kevin.ch.hou@gmail.com>
2016-05-23 22:49:21 -0700

    Added more LEDs

--- Internet_Controlled_LEDs.ino
+++ Internet_Controlled_LEDs.ino
@@ -5,8 +5,8 @@
 String HttpHeader = String(MaxHeaderLength); 
 
 String tasks[] = {"Kevin", "Hou"}; // Names of tasks - corresponds with LEDs
-int leds[] = {0, 1}; // Led pins
-boolean ledStates[] = {true, false}; // On or off
+int leds[] = {2, 3, 4, 5, 6, 7}; // Led pins
+boolean ledStates[] = {false, false, false, false, false, false}; // On or off
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; // Physical mac address
 byte ip[] = { 192, 168, 2, 201 }; // Static IP of Arduino
@@ -28,9 +28,11 @@ void setup(){
   // Start Ethernet
   Ethernet.begin(mac, ip, gateway, subnet);
   if (Ethernet.begin(mac)) {
-    Serial.println(F("Connected."));
+//    Serial.println(F("Connected."));
     Serial.print(F("Point your Browser at: http://"));
     Serial.println(Ethernet.localIP());
+    Serial.println("Flashing");
+    flashLEDs(); // Flash leds
   } else {
     Serial.println(F("Could not connect to network"));
   }
@@ -58,7 +60,7 @@ void loop(){
            tempURL = HttpHeader;
 //           Serial.println(tempURL); // Feedback
            if (tempURL.indexOf('?') != -1) {
-             Serial.println("Actual URL:");
+//             Serial.println("Actual URL:");
              URL = tempURL;
              Serial.println(URL);
              parseURL();
@@ -67,14 +69,28 @@ void loop(){
           // start of web page
            client.println("HTTP/1.1 200 OK");
            client.println("Content-Type: text/html");
+//           delay(5);
            client.println("<html><head></head><body>");
+//           delay(5);
            client.println();
            client.print("<form method=get>");
+//           delay(5);
            client.print("<input type='checkbox' name=r value='led0'> Item One<br>");
-           client.print("<input type='checkbox' name=r value='led1' checked> Item Two<br>");
+//           delay(5);
+           client.print("<input type='checkbox' name=r value='led1'> Item Two<br>");
+//           delay(5);
            client.print("<input type='checkbox' name=r value='led2'> Item Three<br>");
-            client.print("<input type=submit value=submit></form>");
+//           delay(5);
+           client.print("<input type='checkbox' name=r value='led3'> Item Four<br>");
+           delay(5);
+           client.print("<input type='checkbox' name=r value='led4'> Item Five<br>");
+//           delay(5);
+           client.print("<input type='checkbox' name=r value='led5'> Item Six<br>");
+//           delay(5);
+           client.print("<input type=submit value=submit></form>");
+//           delay(5);
            client.print("</body></html>");
+//           delay(5);
            //clearing string for next read
            HttpHeader="";
            // Stopping client
@@ -87,8 +103,8 @@ void loop(){
 
 void parseURL() {
   for (int i = 0; i < sizeof(leds); i++) {
-    String query = "r=" + leds[i]; // Search query
-    Serial.println("Looking for " + query); // Feedback
+    String query = "r=led" + String(i); // Search query
+//    Serial.println("Looking for " + query); // Feedback
     int result = URL.indexOf(query);
     if (result != -1) { // If contains the LED number in the URL
       Serial.println("LED: " + String(i) + " on"); // Feedback
@@ -102,12 +118,25 @@ void parseURL() {
 
 void setLEDs() {
   for (int i = 0; i < sizeof(ledStates); i++) {
+    Serial.println("LED Pin: " + String(leds[i]) + " is " + String(ledStates[i]));
     if (ledStates[i]) { // If LED is turned on
       digitalWrite(leds[i], HIGH); // Turn on the LED
     } else {
       digitalWrite(leds[i], LOW); // Turn off the LED
     }
   }
+}
+
+void flashLEDs() {
+  for (int i = 0; i < sizeof(ledStates); i++) {
+    digitalWrite(leds[i], HIGH); // Turn on the LED
+    Serial.println("Turning on LED: " + leds[i]);
+  }
+  delay(2000);
+  for (int i = 0; i < sizeof(ledStates); i++) {
+    digitalWrite(leds[i], LOW); // Turn on the LED
+  }
+  delay(500);
 }
 
 void inititateLEDs() {
